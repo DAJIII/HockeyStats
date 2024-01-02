24051456c1d01e14e2f4c85a4499552eb0a3b216
DAJIII <ajohnston.bh@gmail.com>
2024-01-02 06:16:14 -0500

    Updating DB to Jan 1, 2024

--- Batch Loads and Transfers/BatchStatScraperPlayer.py
+++ Batch Loads and Transfers/BatchStatScraperPlayer.py
@@ -5,10 +5,10 @@ import time
 
 conn = sqlite3.connect('HockeyStats.db')
 
-statdate = dt.datetime(2023,10,10)
+statdate = dt.datetime(2023,12,31)
 count = 0
 
-while statdate <= dt.datetime(2023, 12, 31):
+while statdate <= dt.datetime(2024, 1, 1):
     statday = statdate.strftime('%Y-%m-%d')
     staturl = f'https://www.naturalstattrick.com/playerteams.php?fromseason=20232024&thruseason=20232024&stype=2&sit=all&score=all&stdoi=std&rate=n&team=ALL&pos=S&loc=B&toi=0&gpfilt=gpdate&fd={statday}&td={statday}&tgp=410&lines=single&draftteam=ALL'
     dailydf = pd.read_html(staturl)
